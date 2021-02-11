0ae9b0ae0ead741b7b3f0a5573b9386ff3e0b2ae
Zachary McCain <zacmmcca@gmail.com>
2021-02-11 13:50:56 -0600

    Styled with grid and image on left.

--- socialCard/client/SocialCard.jsx
+++ socialCard/client/SocialCard.jsx
@@ -4,10 +4,15 @@ const SocialCard = ({user}) => {
   return (
     <div className='cardContainer float'>
       <div className="card">
-        <a className='logo' href={user.gitHub} target="_blank"><img src="./../assets/gitHub.png" /></a>
+        <a className='logo' href={user.gitHub} target="_blank">
+          <img src="./../assets/IMG_9001.jpg" />
+        </a>
         <div className='user'>
           <h1 className='name'>{user.name}</h1>
           <h5 className='title'>{user.title}</h5>
+        </div>
+        <div className='info'>
+          <p>I am writing some information that is intended to go in the bottom right-hand corner of the card.</p>
         </div>
       </div>
     </div>
