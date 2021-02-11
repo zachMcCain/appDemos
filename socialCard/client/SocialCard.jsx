8ff219ef455494d6908131783d00e7045e51b031
Zachary McCain <zacmmcca@gmail.com>
2021-02-11 10:11:39 -0600

    Updated boilerplate for SocialCard.

---
+++ socialCard/client/SocialCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SocialCard = (props) => {
+  return (
+    <div>
+      <img src="./../assets/gitHub.png" />
+      <h1>Social Card</h1>
+    </div>
+  )
+}
+
+export default SocialCard;
---
+++ socialCard/client/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SocialCard from './SocialCard.jsx';
+
+ReactDOM.render(<SocialCard />, document.getElementById('app'));
